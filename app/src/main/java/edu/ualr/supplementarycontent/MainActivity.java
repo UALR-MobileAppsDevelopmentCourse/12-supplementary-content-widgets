9750abbbf33638aa360e3e354dd97057501a640f
irconde <ivanrodriguezconde@gmail.com>
2019-10-17 22:26:06 -0500

    appbar-setup

---
+++ app/src/main/java/edu/ualr/supplementarycontent/MainActivity.java
@@ -0,0 +1,22 @@
+package edu.ualr.supplementarycontent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+
+// TODO 02. Make sure the activity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO 04. Set the toolbar as the app bar for the activity
+        View view;
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
