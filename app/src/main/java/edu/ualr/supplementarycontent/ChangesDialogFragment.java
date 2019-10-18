a2e35f7f304da5fe1eaaaf14cc67fe18d3b97748
irconde <ivanrodriguezconde@gmail.com>
2019-10-17 22:26:06 -0500

    appbar setup

---
+++ app/src/main/java/edu/ualr/supplementarycontent/ChangesDialogFragment.java
@@ -0,0 +1,26 @@
+package edu.ualr.supplementarycontent;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+/**
+ * Created by irconde on 2019-10-18.
+ */
+
+// TODO 02. We must create a class that extends the DialogFragment class
+public class ChangesDialogFragment{
+     // TODO 03. We must override the onCreateDialog method
+        // TODO 04. We use the Builder class to construct the new dialog
+        // TODO 05. We set the message text and the title of the dialog...
+        // TODO 07. We define a button the user should use to accept and continue with the action
+        // TODO 08. We define a button the user should use to cancel the action
+        // TODO 09. Create the dialog object and return it
+}
---
+++ app/src/main/java/edu/ualr/supplementarycontent/MainActivity.java
@@ -0,0 +1,27 @@
+package edu.ualr.supplementarycontent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+
+// TODO 01. Create a alertDialog managed by within a DialogFragment
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String FRAGMENT_TAG = "SaveDialog";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void showDialog(View view) {
+        // TODO 10. Create an instance of the dialog
+        // TODO 11. Show the dialog
+    }
+}
