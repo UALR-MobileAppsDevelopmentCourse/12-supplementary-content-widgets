c156fa0c95592f18439c5fa58681044f045e5b08
irconde <ivanrodriguezconde@gmail.com>
2019-10-17 23:14:07 -0500

    Defining appbar's menu

--- app/src/main/java/edu/ualr/supplementarycontent/MainActivity.java
+++ app/src/main/java/edu/ualr/supplementarycontent/MainActivity.java
@@ -1,22 +1,49 @@
 package edu.ualr.supplementarycontent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
-// TODO 02. Make sure the activity extends AppCompatActivity
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO 04. Set the toolbar as the app bar for the activity
-        View view;
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    // TODO 03. Specify the options menu for the activity.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    // TODO 04. Handling click events
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.favourite_action:
+                Log.d(TAG, "Favourite option clicked");
+                return true;
+            case R.id.delete_action:
+                Log.d(TAG, "Delete option clicked");
+                return true;
+            case R.id.settings_action:
+                Log.d(TAG, "Settings option clicked");
+                return true;
+                default: return super.onOptionsItemSelected(item);
+        }
     }
 }
