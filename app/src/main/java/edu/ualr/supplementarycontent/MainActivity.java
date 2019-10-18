3619f19105238ab6c16285ce48ea8fe3012519f7
irconde <ivanrodriguezconde@gmail.com>
2019-10-17 22:26:06 -0500

    appbar setup

---
+++ app/src/main/java/edu/ualr/supplementarycontent/MainActivity.java
@@ -0,0 +1,32 @@
+package edu.ualr.supplementarycontent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+// TODO 01: Define a new xml menu resource to add actions to the action bar. toolbar_menu.xml
+
+// TODO 02: We have to add an item element for each item you want to include in the action bar
+/**
+ * "Mark Favorite", should appear as action button always
+ * "Delete", should appear as action button if possible
+ * "Settings", should not appear as action button
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    // TODO 03. Specify the options menu for the activity. Override the onCreateOptionsMenu method
+
+    // TODO 04. Handling click events. Override onOptionsItemSelected method
+
+}
