b4d975ba3a5e624c7f138fffc66e6963c673ab85
irconde <ivanrodriguezconde@gmail.com>
2019-10-18 21:58:52 -0500

    Custom toast implementation

--- app/src/main/java/edu/ualr/supplementarycontent/MainActivity.java
+++ app/src/main/java/edu/ualr/supplementarycontent/MainActivity.java
@@ -9,9 +9,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -55,16 +58,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showToast(View view) {
-        // TODO 01. Instantiate a Toast object.
-        // We must provide the application context, the text message, and the duration of the toast
         Context context = getApplicationContext();
         CharSequence text = getResources().getString(R.string.toast_message);
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
-        // TODO 02. We define the position of the toast
-        // By default, it's positioned in the bottom center of the screen
         toast.setGravity(Gravity.TOP, 0, 128);
-        // TODO 03. We display the toast notification
+        toast.show();
+    }
+
+    // TODO 01. Define a customized layout for the toast notification
+    public void showCustomToast(View view) {
+        // TODO 02. We retrieve the layout inflater
+        LayoutInflater inflater = getLayoutInflater();
+        // TODO 03. Inflate the toast layout and create the corresponding view
+        View layout = inflater.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.custom_toast_container));
+        // TODO 04. We set the text of the custom toast
+        TextView text = (TextView) layout.findViewById(R.id.text);
+        text.setText("This is a custom toast");
+        Toast toast = new Toast(getApplicationContext());
+        // TODO 05. We can set toast's gravity and duration
+        toast.setGravity(Gravity.BOTTOM, 0, 32);
+        toast.setDuration(Toast.LENGTH_LONG);
+        // TODO 06. We plug in the view in the toast
+        toast.setView(layout);
         toast.show();
     }
 }
